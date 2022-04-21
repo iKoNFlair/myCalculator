85aff03d16b0033d892dbb7dc636210a0b2874ab
Ritik Pillai <102801379+iKoNFlair@users.noreply.github.com>
2022-04-21 17:41:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let screen = document.querySelector("#screen");
+let btn = document.querySelectorAll(".btn");
+let screenValue = '';
+
+console.log("btn =" ,btn.length);
+
+for(items of btn)
+{   
+    items.addEventListener("click", (e) => {
+    
+    e.target.classList.toggle("btn-click");
+    setTimeout(() => {
+        e.target.classList.toggle("btn-click");
+    }, 100);
+
+    let buttonText = e.target.innerText;
+    calc(buttonText);
+    })
+}
+
+document.addEventListener("keypress", (e) => {
+   
+//    for(let i=0; i<btn.length; i++)
+//    {
+//        classList.toggle("btn-click");
+//        setTimeout(() => {
+//            e.target.classList.toggle("btn-click");
+//        }, 100);
+//    }
+
+console.log(e.key);
+calc(e.key);
+
+
+})
+
+function calc(key){
+    if(key == 'X' || key == '*')
+    {
+        key = '*';
+        screenValue += key;
+        screen.value = screenValue;
+    }
+  else if(key == 'C' || key == 'c')
+  {
+      screenValue = '';
+      screen.value = 0;
+  }
+  else if(key == '=' || key == "Enter")
+  {
+      screenValue = eval(screenValue);
+      screen.value = screenValue;
+  }
+  else if(key == 'Del' || key == 'd' || key == 'D')
+  {
+      screenValue = screenValue.slice(0,-1);
+      screen.value = screenValue;
+  }
+  else {
+      screenValue += key;
+      screen.value = screenValue;
+  }
+}
