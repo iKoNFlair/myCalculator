2ad00c0add34c72e0935bfb3625fdfcf02b37173
Ritik Pillai <102801379+iKoNFlair@users.noreply.github.com>
2022-05-01 23:34:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ let screen = document.querySelector("#screen");
 let btn = document.querySelectorAll(".btn");
 let screenValue = '';
 
+let switchBtn = document.querySelector(".btn-switch");
+let container = document.querySelector(".container");
+let scientificBtns = document.querySelectorAll(".scientific");
+
+switchBtn.addEventListener("click", function(){
+    container.classList.toggle('max-container');
+   for(let i=0; i<scientificBtns.length; i++)
+    {
+        scientificBtns[i].classList.toggle("scientific-unhide");
+    }
+})
+
 console.log("btn =" ,btn.length);
 
 for(items of btn)
@@ -19,7 +31,19 @@ for(items of btn)
 }
 
 document.addEventListener("keypress", (e) => {
-   calc(e.key);
+   
+//    for(let i=0; i<btn.length; i++)
+//    {
+//        classList.toggle("btn-click");
+//        setTimeout(() => {
+//            e.target.classList.toggle("btn-click");
+//        }, 100);
+//    }
+
+console.log(e.key);
+calc(e.key);
+
+
 })
 
 function calc(key){
@@ -29,28 +53,36 @@ function calc(key){
         screenValue += key;
         screen.value = screenValue;
     }
-  else if(key == 'C' || key == 'c')
+    else if(key == 'C' || key == 'c')
   {
       screenValue = '';
       screen.value = 0;
   }
-  else if(key == '=' || key == "Enter")
+    else if(key == '=' || key == "Enter")
   {
       screenValue = eval(screenValue);
       screen.value = screenValue;
+
+    //   if()
   }
-  else if(key == 'Del' || key == 'd' || key == 'D')
-  {
+    else if(key == 'Del' || key == 'd' || key == 'D')
+  {   
       screenValue = screenValue.slice(0,-1);
       screen.value = screenValue;
   }
-   else if(key == '%')
-   {
-      screenValue /= 100;
-      screen.value = screenValue;
-   }
-   else {
+   else if(key == "Sin")
+     {   
+        //  screenValue = Math.sin(screenValue*Math.PI/180);
+         screenValue += "Sin";
+         screen.value = screenValue;
+     }
+  else {
       screenValue += key;
       screen.value = screenValue;
   }
 }
+
+let x=45;
+console.log(Math.sin(x*Math.PI/180))
+console.log(Math.cos(x*Math.PI/180))
+console.log(Math.tan(x*Math.PI/180))
